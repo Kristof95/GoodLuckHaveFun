a833e1a9f59f6e24b4ed32049e5c4f298861c8d1
Vas Richard <kyler50vasrich@gmail.com>
2015-11-12 11:08:41 +0100

    random ID,illetve delete.

--- donation_location.py
+++ donation_location.py
@@ -240,6 +240,18 @@ class Donation_class:
             writer.writerow(data)
 
 
+def delete_donations_from_csv_file():
+    according_ID = input("Add ID which donation you want to delete:")
+    delete_from_csv = open("Data\donations.csv", "r+")
+    read_cs_line = delete_from_csv.readlines()
+    delete_from_csv.seek(0)
+    for i in read_cs_line:
+        if according_ID not in i:
+            delete_from_csv.write(i)
+    delete_from_csv.truncate()
+    delete_from_csv.close()
+
+
 
 
 
